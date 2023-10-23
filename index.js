75b77ae4214edc26c4c272a43308c3dcce8cea1b
谭正飞 <zhengfei.tan@definesys.com>
2023-10-23 23:45:21 +0800

    调整输出

--- index.js
+++ index.js
@@ -4,11 +4,12 @@
  * @Date: 2023-10-23 22:23:36
  * @FilePath: \express-app\index.js
  * @LastEditors: zhengfei.tan
- * @LastEditTime: 2023-10-23 23:36:40
+ * @LastEditTime: 2023-10-23 23:44:52
  */
 const express = require('express')
 const app = express()
 const schedule = require('node-schedule');
+const utils = require('./utils/index')
 
 
 const testRouter = require('./models/test')
@@ -19,14 +20,15 @@ app.get('/', (req, res) => {
 })
 
 
+
 // 定义定时任务
 function job() {
   // 输出当前格式化后的时间（yyyy-MM-dd）
-  console.log('定时任务执行，执行一些操作,当前时间为：', new Date().toLocaleString());
+  console.log('定时任务执行，执行一些操作,当前时间为：', utils.formatDate(new Date()) );
  }
 
 // 创建一个定时任务，每隔 1小时 执行一次 job 函数
-schedule.scheduleJob('*/1 * * * *', job);
+schedule.scheduleJob('1 * * * *', job);
 
 
 app.listen(3000, () => {
